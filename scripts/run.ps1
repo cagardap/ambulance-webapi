8073d5501d23d9d17d0a08d8a0aac11c451eac66
Peter Cagarda <peter.cagarda@siemens-healthineers.com>
2024-02-06 16:38:09 +0100

    dockerization

--- scripts/run.ps1
+++ scripts/run.ps1
@@ -35,6 +35,9 @@ switch ($command) {
     "mongo" {
       mongo up
     }
+    "docker" {
+      docker build -t cagardap/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/DockerFile .
+    }
     default {
         throw "Unknown command: $command"
     }
